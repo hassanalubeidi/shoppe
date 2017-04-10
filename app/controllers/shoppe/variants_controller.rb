8819a32698a47424d1cea1d11f3f1e44cbff620b
hassanalubeidi <hassanalubeidi1996@gmail.com>
2017-04-10 11:44:55 +0100

    attachment params

--- app/controllers/shoppe/variants_controller.rb
+++ app/controllers/shoppe/variants_controller.rb
@@ -47,7 +47,7 @@ module Shoppe
     private
 
     def safe_params
-      params[:product].permit(:name, :permalink, :sku, :default_image_file, :price, :cost_price, :tax_rate_id, :weight, :stock_control, :active, :default, product_attributes_array: [:key, :value, :searchable, :public, :customizable])
+      params[:product].permit(:name, :permalink, :sku, :default_image_file, :price, :cost_price, :tax_rate_id, :weight, :stock_control, :active, :default, product_attributes_array: [:key, :value, :searchable, :public, :customizable], attachments: [default_image: file_params, data_sheet: file_params, extra: file_params])
     end
   end
 end
