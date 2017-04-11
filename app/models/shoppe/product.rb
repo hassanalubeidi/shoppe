5e8ffcbfb83fa00334848fa4e5d69745dbb83141
hassanalubeidi <hassanalubeidi1996@gmail.com>
2017-04-11 13:30:21 +0100

    paperclip

--- app/models/shoppe/product.rb
+++ app/models/shoppe/product.rb
@@ -3,6 +3,7 @@ require 'globalize'
 
 module Shoppe
   class Product < ActiveRecord::Base
+    has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
     self.table_name = 'shoppe_products'
 
     # Add dependencies for products
